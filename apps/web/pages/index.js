d1f7ba1587857eca8b24dc99e03b2b3d8c500c0a
rwbgoachic <rwbchicago@gmail.com>
2025-06-11 07:48:13 -0500

    Create index.js

---
+++ apps/web/pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>🚀 PaySurity Platform Placeholder UI</h1>
+    </div>
+  )
+}
